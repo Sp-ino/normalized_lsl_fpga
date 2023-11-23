df85e51a70b40a20e55a29b8a39ce482b48752cd
sp-ino <spinovale97@gmail.com>
2023-11-24 00:27:04 +0100

    Remove residual file

--- normalized_lsl_fpga.tcl
+++ normalized_lsl_fpga.tcl
@@ -30,7 +30,6 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/src/postmul_resizer_wideinput.vhd"]"\
  "[file normalize "$origin_dir/src/normalize.vhd"]"\
  "[file normalize "$origin_dir/src/divider.vhd"]"\
- "[file normalize "$origin_dir/vivado_project/normalized_lsl_fpga.srcs/sources_1/imports/hdl/init_chi_wrapper.vhd"]"\
  "[file normalize "$origin_dir/src/scalar_delay.vhd"]"\
  "[file normalize "$origin_dir/src/tb/update_gamma_tb.vhd"]"\
  "[file normalize "$origin_dir/src/wave_config/tb_behav_gamma.wcfg"]"\
@@ -196,7 +195,6 @@ set files [list \
  [file normalize "${origin_dir}/src/postmul_resizer_wideinput.vhd"] \
  [file normalize "${origin_dir}/src/normalize.vhd"] \
  [file normalize "${origin_dir}/src/divider.vhd"] \
- [file normalize "${origin_dir}/vivado_project/normalized_lsl_fpga.srcs/sources_1/imports/hdl/init_chi_wrapper.vhd"] \
  [file normalize "${origin_dir}/src/scalar_delay.vhd"] \
 ]
 add_files -norecurse -fileset $obj $files
@@ -258,11 +256,6 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
 set file "$origin_dir/src/divider.vhd"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "VHDL" -objects $file_obj
-
-set file "$origin_dir/vivado_project/normalized_lsl_fpga.srcs/sources_1/imports/hdl/init_chi_wrapper.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
